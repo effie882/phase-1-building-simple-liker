428bf94ff204e86fc9913ab378a32633fc62315a
effie882 <effiemorara@gmail.com>
2025-06-29 14:07:17 +0300

    initial commit

--- main.js
+++ main.js
@@ -2,7 +2,32 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
-// Your JavaScript code goes here!
+const modal = document.getElementById('modal');
+modal.classList.add('hidden');
+
+const hearts = document.querySelectorAll('.like-glyph');
+
+hearts.forEach(heart => {
+  heart.addEventListener('click', () => {
+    mimicServerCall()
+      .then(() => {
+        if (heart.textContent === EMPTY_HEART) {
+          heart.textContent = FULL_HEART;
+          heart.classList.add('activated-heart');
+        } else {
+          heart.textContent = EMPTY_HEART;
+          heart.classList.remove('activated-heart');
+        }
+      })
+      .catch(error => {
+        modal.classList.remove('hidden');
+        document.getElementById('modal-message').textContent = error;
+        setTimeout(() => {
+          modal.classList.add('hidden');
+        }, 3000);
+      });
+  });
+});
 
 
 
